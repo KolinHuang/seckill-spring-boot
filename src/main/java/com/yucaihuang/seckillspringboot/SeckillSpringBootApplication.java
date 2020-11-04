9802ace4f5c6561f36440ffb55eccf92170feaf5
黄玉才 <yc_huang97@163.com>
2020-11-04 16:07:39 +0800

    spring-boot 实现简单的秒杀系统

---
+++ src/main/java/com/yucaihuang/seckillspringboot/SeckillSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yucaihuang.seckillspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yucaihuang/seckillspringboot/SeckillSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yucaihuang.seckillspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
