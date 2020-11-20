e7b1a30b3563a4e865ad686f26d82d8a77b1b9f9
黄玉才 <yc_huang97@163.com>
2020-11-20 15:34:50 +0800

    rabbitMQ异步发送邮件

---
+++ src/main/java/com/yucaihuang/seckillspringboot/dto/MailDto.java
@@ -0,0 +1,25 @@
+package com.yucaihuang.seckillspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author kol Huang
+ * @date 2020/11/20
+ */
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailDto {
+
+    //邮件主题
+    private String subject;
+    //邮件内容
+    private String content;
+    //接收人
+    private String[] tos;
+}
